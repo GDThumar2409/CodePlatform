20d6e42e5164ac8bb328775a6eadd7ca91aaad8c
Your Name <you@example.com>
2020-04-10 17:56:58 +0000

    frontend CSV and Router

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import Header from './Header';
-
+import Home from './Home'
+import CSVUpload from './CSVUpload'
+import './style.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'React'
+      name: 'Home'
     };
   }
-
   render() {
     return (
+      <div>
         <Header />
+        <Switch>
+        {/* If the current URL is /about, this route is rendered
+            while the rest are ignored */}
+        <Route path="/about">
+          <Home />
+        </Route>
+        <Route path="/csvupload">
+          <CSVUpload />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+      </div>
     );
   }
 }
-export default App;
-render(<App />, document.getElementById('root'));
+
+render(<Router forceRefresh={false}><App /></Router>, document.getElementById('root'));
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import Header from './Header';
-
+import Home from './Home'
+import CSVUpload from './CSVUpload'
+import './style.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'React'
+      name: 'Home'
     };
   }
-
   render() {
     return (
+      <div>
         <Header />
+
+      </div>
     );
   }
 }
